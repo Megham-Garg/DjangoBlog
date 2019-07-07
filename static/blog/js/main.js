b72b991bed5eb30f093f5449e33cce2d5c5ddf03
Megham-Garg <gargmegham3@gmail.com>
2019-07-07 15:19:54 +0530

    07.07.2019

---
+++ static/blog/js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $("#id_image").replaceWith("<div class='custom-file col-md-4 mt-3 ml-2'><input type='file' name= 'image' accept='image/*' class='clearablefileinput custom-file-input' id='customFile id_image'><label class='custom-file-label' for='customFile'>Choose file</label></div>");
+});
